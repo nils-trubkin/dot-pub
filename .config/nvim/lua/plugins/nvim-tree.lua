111ebac2c2cc96cc86e217cc1ad4c51f11a5ef9d
Nils Trubkin <32000568+nils-trubkin@users.noreply.github.com>
2026-06-02 21:59:30 +0200

    Update nvim-tree config

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -3,6 +3,12 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local tree = require("nvim-tree")
+    local screen_height = vim.o.lines
+    local screen_width = vim.o.columns
+    local height = math.floor(screen_height * 0.75)
+    local width = math.floor(screen_width * 0.60)
+    local row = math.floor((screen_height - height) * 0.5)
+    local col = math.floor((screen_width - width) * 0.5)
 
     tree.setup({
       bookmarks = {
@@ -12,7 +18,20 @@ return {
           sorter = "case_sensitive",
       },
       view = {
-        width = 35,
+        centralize_selection = true,
+        number = true,
+        relativenumber = true,
+        -- float = {
+        --   enable = true,
+        --   open_win_config = {
+        --     relative = "editor",
+        --     border = "none",
+        --     width  = width,
+        --     height = screen_height,
+        --     row = 0,
+        --     col = col,
+        --   },
+        -- },
       },
       filters = {
       },
