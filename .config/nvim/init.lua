8fec5be131107f75fc9d8afeecf1b14165d08d39
Nils Trubkin <32000568+nils-trubkin@users.noreply.github.com>
2026-06-21 20:45:47 +0200

    Add hardtime.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,6 +6,7 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 -- ========== VIM OPTIONS ==========
+vim.opt.showmode = false
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
